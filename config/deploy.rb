4ba8b669675c7b2373f0567e3e31ba493bf7982c
kenshero <kenshero.k@gmail.com>
2016-01-12 23:44:00 +0700

    update cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,12 +3,12 @@ lock '3.4.0'
 
 set :application, 'prejournal'
 set :repo_url, 'git@github.com:kenshero/prejournal.git'
-
+set :rbenv_path, '/home/ejadmin/.rbenv/'
 set :ssh_options, { :forward_agent => true }
 set :deploy_to, '/home/ejadmin/prejournal'
 set :deploy_via, :copy
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 
